d2a7b4f2152946dc85f29e53f07e7a64ceca4d19
JondareHM <Jacob_h@live.dk>
2019-12-09 16:43:24 +0100

    begyndelse på selve siden

--- jobtest/film/models.py
+++ jobtest/film/models.py
@@ -6,3 +6,5 @@ class Movie(models.Model):
 	genre	= models.CharField(max_length=200)
 	poster	= models.CharField(max_length=200) #todo bedre måde at gøre billedet på med API
 	media	= models.CharField(max_length=200)
+	def __str__(self):
+		return self.title
--- jobtest/film/views.py
+++ jobtest/film/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+def detail(request, movie_id):
+    return HttpResponse("You're looking at the movie %s." % question_id)
