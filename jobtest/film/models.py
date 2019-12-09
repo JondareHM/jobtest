51307789dd5d8be551f25275c9ef455445373162
JondareHM <Jacob_h@live.dk>
2019-12-09 16:26:27 +0100

    Projekt setup og første iteration af database

---
+++ jobtest/film/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+class Movie(models.Model):
+	title	= models.CharField(max_length=200)
+	genre	= models.CharField(max_length=200)
+	poster	= models.CharField(max_length=200) #todo bedre måde at gøre billedet på med API
+	media	= models.CharField(max_length=200)
