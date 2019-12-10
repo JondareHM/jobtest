ea24096c2219ed27636a4f6eb745a60f285fdc06
JondareHM <Jacob_h@live.dk>
2019-12-10 15:06:18 +0100

    Fik API kald og form til at virke.
    TODO:
    
    - Gemme data fra API kald i DB (evt. spørge om data er korrekt først?)
    - Tests

--- jobtest/film/views.py
+++ jobtest/film/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
 from .models import Movie
+import json
+import requests
 
 def index(request):
     movie_list = Movie.objects.order_by('id')
@@ -10,3 +12,12 @@ def index(request):
 def detail(request, id):
     movie = get_object_or_404(Movie, pk=id)
     return render(request, 'film/detail.html', {'movie': movie})
+	
+def register(request):
+    movie = {}
+    if 'title' in request.GET:
+        title = request.GET['title']
+        url = 'http://www.omdbapi.com/?apikey=4e898dbb&t={0}'.format(title)
+        response = requests.get(url)
+        movie = response.json()
+    return render(request, 'film/register.html', {'movie': movie})
