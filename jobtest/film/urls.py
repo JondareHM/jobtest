dd3e3a58ddae7edb6c34465aa8b5fe12043d9428
JondareHM <Jacob_h@live.dk>
2019-12-11 15:25:28 +0100

    Feature complete
    TODO:
    -skrive tests
    -virker lige nu kun med titler på præcist et ord, skal have fikset det.

--- jobtest/film/urls.py
+++ jobtest/film/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
 	path('<int:id>/', views.detail, name='detail'),
+	path('search/', views.search, name='search'),
 	path('register/', views.register, name='register'),
 ]
--- jobtest/film/views.py
+++ jobtest/film/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, render, redirect
 from django.http import HttpResponse
+from django.template import RequestContext
 from .models import Movie
 import json
 import requests
@@ -13,11 +14,25 @@ def detail(request, id):
     movie = get_object_or_404(Movie, pk=id)
     return render(request, 'film/detail.html', {'movie': movie})
 	
+def search(request):
+	return render(request, 'film/search.html', {})
+	
 def register(request):
-    movie = {}
-    if 'title' in request.GET:
-        title = request.GET['title']
-        url = 'http://www.omdbapi.com/?apikey=4e898dbb&t={0}'.format(title)
-        response = requests.get(url)
-        movie = response.json()
-    return render(request, 'film/register.html', {'movie': movie})
+	movie = {}
+	global movie_title
+	global movie_genre
+	global movie_poster
+	if 'title' in request.POST:
+		title = request.POST.get('title', '')
+		url = 'http://www.omdbapi.com/?apikey=4e898dbb&t={0}'.format(title)
+		response = requests.get(url)
+		movie = response.json()
+		movie_title = movie['Title']
+		movie_genre = movie['Genre']
+		movie_poster = movie['Poster']
+	if 'media' in request.POST:
+		movie_media = request.POST.get('media')
+		new_movie = Movie(title=movie_title, genre=movie_genre, poster=movie_poster, media=movie_media)
+		new_movie.save()
+		return redirect('index')
+	return render(request, 'film/register.html', {'movie': movie})
